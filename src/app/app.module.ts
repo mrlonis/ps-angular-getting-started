5713a903a38ac5515a2a1fe970d4d609c2a9e598
Matthew Lonis <mattlonis@gmail.com>
2021-04-28 15:08:56 -0400

    More on Components
    Building Custom Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pipes/convert-to-spaces.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertToSpacesPipe } from './convert-to-spaces.pipe';
+
+describe('ConvertToSpacesPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertToSpacesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
